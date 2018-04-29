89c426d7457661bd59adf20b87d6685f041a59b2
Wanasak <u510610433@gmail.com>
2018-04-29 14:41:00 +0700

    action filters

--- Startup.cs
+++ Startup.cs
@@ -56,6 +56,7 @@ namespace DatingApp.API
             services.AddMvc().AddJsonOptions(options => {
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             });
+            services.AddScoped<LoggedUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
